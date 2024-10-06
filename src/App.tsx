6493f9d93aa06b2f3a190140a2957ffaba774139
Raryson Rost <raryson@DESKTOP-F39H87A>
2024-10-05 22:31:54 -0300

    feat: add testint library

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <Search />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
