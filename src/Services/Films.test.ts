e701e61cd37ecbd9bb2cd13ae16d2eabe8146602
Raryson Rost <raryson.rost@gmail.com>
2024-10-07 16:41:12 -0300

    test: add tests to http client

--- src/Services/Films.test.ts
+++ src/Services/Films.test.ts
@@ -1,10 +1,34 @@
-import { getFilmsByName } from './Films';
+import { getFilmsByName, getFilmsByUrl } from './Films';
 import { httpClient } from '../Infra/httpClient';
 
 describe('getFilmsByName', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  const mockResponse = {
+    ok: true,
+    status: 200,
+    statusText: 'OK',
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+    redirected: false,
+    url: 'https://example.com',
+    json: async () => Promise.resolve({ data: 'test' }),
+  } as Response;
+  
+
   it('should call films?search=Empire', async () => {
     httpClient.get = jest.fn().mockResolvedValue({ data: {} });
     await getFilmsByName('Empire');
     expect(httpClient.get).toHaveBeenCalledWith('films?search=Empire');
+  });
+
+  it('should call entire url', async () => {
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() =>
+      Promise.resolve(mockResponse)
+    ) as jest.Mock;
+    const fakeUrl = 'https://swapi.dev/api/films';
+    await getFilmsByUrl(fakeUrl);
+    expect(global.fetch).toHaveBeenCalled();
   });
 });
--- src/Services/People.test.ts
+++ src/Services/People.test.ts
@@ -1,10 +1,33 @@
 import { httpClient } from '../Infra/httpClient';
-import { getPeopleByName } from './People';
+import { getPeopleByName, getPeopleByUrl } from './People';
 
 describe('getPeopleByName', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  const mockResponse = {
+    ok: true,
+    status: 200,
+    statusText: 'OK',
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+    redirected: false,
+    url: 'https://example.com',
+    json: async () => Promise.resolve({ data: 'test' }),
+  } as Response;
+  
   it('should call people?search=Yoda', async () => {
     httpClient.get = jest.fn().mockResolvedValue({ data: {} });
     await getPeopleByName('Yoda');
     expect(httpClient.get).toHaveBeenCalledWith('people?search=Yoda');
+  });
+
+  it('should call entire url', async () => {
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() =>
+      Promise.resolve(mockResponse)
+    ) as jest.Mock;
+    const fakeUrl = 'https://swapi.dev/api/people';
+    await getPeopleByUrl(fakeUrl);
+    expect(global.fetch).toHaveBeenCalled();
   });
 });
